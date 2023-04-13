5b0dbcd7c355c6931fa4af7fbf84d42c2764f059
James Olsen <olsenjames1116@gmail.com>
2023-04-13 17:52:17 -0400

    Display user info in header
    The user info that is retrieved from Google Auth is displayed in the
    header.

--- src/header/Account.js
+++ src/header/Account.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LogInButton from './loggedOut/LogInButton';
 import { useSelector } from 'react-redux';
 import UserImage from './loggedIn/UserImage';
+import UserName from './loggedIn/UserName';
 
 export default function Account() {
   const isLoggedIn = useSelector((state) => state.isLoggedIn.value);
@@ -10,6 +11,7 @@ export default function Account() {
     <div className="account">
       {!isLoggedIn && <LogInButton />}
       {isLoggedIn && <UserImage />}
+      {isLoggedIn && <UserName />}
     </div>
   );
 }
--- src/header/loggedIn/UserImage.js
+++ src/header/loggedIn/UserImage.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 export default function UserImage() {
-  const { image } = useSelector((state) => state.userInfo);
-
   return (
     <img className="userImage" src={localStorage.getItem('image')} alt="The user's profile photo" />
   );
