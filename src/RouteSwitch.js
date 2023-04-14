1a9010dafc031ea0e7d7469ecff7fa5772cabfbf
James Olsen <olsenjames1116@gmail.com>
2023-04-14 14:02:08 -0400

    Set up homepage routing

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/content/home/Home';
 
 export default function RouteSwitch() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
