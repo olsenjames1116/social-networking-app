effc51863bf69ded03347e2c2bf4405e69850a1e
James Olsen <olsenjames1116@gmail.com>
2023-04-17 19:56:10 -0400

    Display a formatted runtime
    When a movie page is loaded, the runtime should be displayed in a
    formatted format to appear cleaner to the user.

--- src/components/content/moviePage/movieData/Runtime.js
+++ src/components/content/moviePage/movieData/Runtime.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Runtime() {
-  return <span></span>;
+export default function Runtime({ runtime }) {
+  const formatRuntime = () => {
+    console.log(runtime);
+    const hours = Math.floor(runtime / 60);
+    const minutes = runtime % 60;
+    console.log(hours, minutes);
+
+    return hours > 0 ? `${hours}h ${minutes}m` : `${minutes}m`;
+  };
+
+  return <span>{formatRuntime()}</span>;
 }
+
+Runtime.propTypes = {
+  runtime: PropTypes.number
+};
