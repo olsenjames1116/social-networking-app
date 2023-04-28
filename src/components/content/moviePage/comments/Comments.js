fd45c6e97b2ffdc9a5891d6f390c03e0b24045ce
James Olsen <olsenjames1116@gmail.com>
2023-04-28 18:55:15 -0400

    Create new comment button

--- src/components/content/moviePage/comments/Comments.js
+++ src/components/content/moviePage/comments/Comments.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NewCommentButton from './NewCommentButton';
 
 export default function Comments() {
   return (
     <div className="comments">
       <h2>Comments</h2>
+      <NewCommentButton />
     </div>
   );
 }
---
+++ src/components/content/moviePage/comments/NewCommentButton.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NewCommentButton() {
+  return <button>+Comment</button>;
+}
