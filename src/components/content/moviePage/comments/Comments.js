fb521b98670f4ee8b005711ff0ab2e17420416a1
James Olsen <olsenjames1116@gmail.com>
2023-04-29 17:37:44 -0400

    Hide new comment button if user is not logged in

--- src/components/content/moviePage/comments/Comments.js
+++ src/components/content/moviePage/comments/Comments.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NewCommentButton from './NewCommentButton';
+import { useSelector } from 'react-redux';
 
 export default function Comments() {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn.value);
+
   return (
     <div className="comments">
       <h2>Comments</h2>
-      <NewCommentButton />
+      {isLoggedIn && <NewCommentButton />}
     </div>
   );
 }
