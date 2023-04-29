86ab6380e0feeb71e69499d3718463184f8869e3
James Olsen <olsenjames1116@gmail.com>
2023-04-29 13:24:29 -0400

    Hide popup on close icon click

--- src/components/content/moviePage/comments/popup/CloseIcon.js
+++ src/components/content/moviePage/comments/popup/CloseIcon.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { closeIcon } from '../../../../../images';
+import { useDispatch } from 'react-redux';
+import { hidePopup } from '../../../../../redux/state/popupSlice';
 
 export default function CloseIcon() {
+  const dispatch = useDispatch();
+
   const style = {
     height: '50px',
     width: 'auto'
   };
 
-  return <img src={closeIcon} alt="A close icon" style={style}></img>;
+  return (
+    <img
+      src={closeIcon}
+      alt="A close icon"
+      style={style}
+      onClick={() => dispatch(hidePopup())}></img>
+  );
 }
