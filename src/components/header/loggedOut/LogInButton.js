b2a4e29d5858bdcd4d763736147ef3d399ee5df1
James Olsen <olsenjames1116@gmail.com>
2023-05-01 13:10:41 -0400

    Store new users in firestore

--- src/components/header/loggedOut/LogInButton.js
+++ src/components/header/loggedOut/LogInButton.js
@@ -3,9 +3,38 @@ import { signInWithPopup } from 'firebase/auth';
 import { auth, provider } from '../../../firebase';
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../../redux/state/isLoggedInSlice';
+import { db } from '../../../firebase';
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
 
 export default function LogInButton() {
   const dispatch = useDispatch();
+
+  const validateUser = async () => {
+    try {
+      const docRef = collection(db, 'users');
+      const docQuery = query(docRef, where('id', '==', localStorage.getItem('id')));
+      const docSnap = await getDocs(docQuery);
+
+      if (docSnap.empty) storeNewUser();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const storeNewUser = async () => {
+    console.log('new user');
+    try {
+      const data = {
+        id: localStorage.getItem('id'),
+        name: localStorage.getItem('name')
+      };
+
+      const docRef = collection(db, 'users');
+      await addDoc(docRef, data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const signInWithGoogle = async () => {
     try {
@@ -14,6 +43,8 @@ export default function LogInButton() {
       localStorage.setItem('id', response.user.uid);
       localStorage.setItem('name', response.user.displayName);
       localStorage.setItem('image', response.user.photoURL);
+
+      validateUser();
 
       dispatch(logIn());
     } catch (error) {
