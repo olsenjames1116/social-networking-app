fabdcb51de421a3f05e78586981292d25a7aa4ce
James Olsen <olsenjames1116@gmail.com>
2023-05-07 20:13:19 -0400

    Log user out
    When the user selects to log out from the dropdown account menu, logged
    in features are hidden and local storage is cleared to make way for a
    new user to log in.

--- src/components/header/loggedIn/AccountMenu.js
+++ src/components/header/loggedIn/AccountMenu.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { logOut } from '../../../redux/state/isLoggedInSlice';
 
 export default function AccountMenu() {
+  const dispatch = useDispatch();
+
+  const logOutUser = () => {
+    dispatch(logOut());
+
+    localStorage.clear();
+  };
+
   return (
     <ul className="accountMenu" data-testid="accountMenu">
       <li>Preferences</li>
       <li>Help</li>
       <li>Report Issue</li>
-      <li>Logout</li>
+      <li onClick={() => logOutUser()}>Logout</li>
     </ul>
   );
 }
