e53fc993d16a87fac865eae2820addcb35951f9e
James Olsen <olsenjames1116@gmail.com>
2023-05-07 20:19:21 -0400

    Display author and sources credits

--- src/components/footer/Author.js
+++ src/components/footer/Author.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Author() {
-  return <p>Built and designed by...</p>;
+  return (
+    <p>
+      Built and designed by <a href="https://github.com/olsenjames1116">olsenjames1116</a>
+    </p>
+  );
 }
--- src/components/footer/Sources.js
+++ src/components/footer/Sources.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Sources() {
-  return <p>Sources</p>;
+  return (
+    <p>
+      Images and icons provided by <a href="https://pixabay.com/">Pixabay</a> and{' '}
+      <a href="https://pictogrammers.com/">Pictogrammers</a>
+    </p>
+  );
 }
