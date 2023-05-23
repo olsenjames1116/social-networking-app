07e9507a0a77eeaf7beb0c579d78164e915e53ed
James Olsen <olsenjames1116@gmail.com>
2023-05-23 17:02:25 -0400

    Delete tests

--- src/tests/render.js
+++
@@ -1,7 +0,0 @@
-import { render as rtlRender } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from '../redux/store';
-
-const render = (component) => rtlRender(<Provider store={store}>{component}</Provider>);
-
-export default render;
