39f3d3ed34ee1182800d7634ad1fb3d6dbfa70ee
James Olsen <olsenjames1116@gmail.com>
2023-05-24 15:06:29 -0400

    Change Browser Router to Hash Router
    Routing issues were encountered when the app was deployed. This was
    fixed by changing the routing method.

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Home from './components/content/home/Home/Home';
 import MoviePage from './components/content/moviePage/MoviePage/MoviePage';
 
 export default function RouteSwitch() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<MoviePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
